877ee3635ae17b419a768b3c405a0f7c5e2ec558
Dasigma <delvonf@gmail.com>
2025-02-14 10:59:45 -0600

    insertionSort

---
+++ Algorithms/Sorting.py
@@ -0,0 +1,32 @@
+########## Sorting ##########
+
+def bubbleSort(mylist):
+   for i in range(len(mylist) - 1, 0, -1):
+      for j in range(i):
+         if mylist[j] > mylist[j + 1]:
+            temp = mylist[j]
+            mylist[j] = mylist[j + 1]
+            mylist[j + 1] = temp
+
+def selectionSort(myList):
+   for i in range(len(myList) - 1):
+      minIndex = i
+      for j in range(i + 1, len(myList)):
+         if myList[j] < myList[minIndex]:
+            minIndex = j
+      myList[i], myList[minIndex] = myList[minIndex], myList[i]
+
+def insertionSort(myList):
+   for i in range(1, len(myList)):
+      j = i - 1
+      while myList[j + 1] < myList[j] and j >= 0:
+         myList[j + 1], myList[j] = myList[j], myList[j + 1]
+         j -= 1
+         
+      
+        
+l = ['a','z','n','r','q',]
+# l = [4,2,6,5,1,3]
+print('Unsorted', l)
+insertionSort(l)
+print('Sorted', l)
