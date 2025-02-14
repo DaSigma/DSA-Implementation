6160e1eb7b4f1967ee2956b8effa20d6a6842eee
Dasigma <delvonf@gmail.com>
2025-02-14 11:03:16 -0600

    fix bubbleSort

--- Algorithms/Sorting.py
+++ Algorithms/Sorting.py
@@ -4,9 +4,7 @@ def bubbleSort(mylist):
    for i in range(len(mylist) - 1, 0, -1):
       for j in range(i):
          if mylist[j] > mylist[j + 1]:
-            temp = mylist[j]
-            mylist[j] = mylist[j + 1]
-            mylist[j + 1] = temp
+            mylist[j],mylist[j + 1] = mylist[j + 1], mylist[j]
 
 def selectionSort(myList):
    for i in range(len(myList) - 1):
